69e18f6133a8b8573b256fa71e3b1d621eade93e
Michael <michaelleie0ca@gmail.com>
2023-12-20 19:18:20 -0800

    Add max width wrapper

---
+++ src/components/max-width-wrapper.tsx
@@ -0,0 +1,7 @@
+type MaxWidthWrapperProps = {
+  children: React.ReactNode;
+};
+
+export default function MaxWidthWrapper({ children }: MaxWidthWrapperProps) {
+  return <main className="pt-5 px-5 max-w-xl mx-auto">{children}</main>;
+}
