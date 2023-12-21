8f081d0222be82c0da5628fa521a2b61d5ce4e87
Michael <michaelleie0ca@gmail.com>
2023-12-21 00:29:58 -0800

    Add seed file to seed data into db

---
+++ seed.ts
@@ -0,0 +1,42 @@
+import { db } from "@/db";
+import { posts } from "@/db/schema/posts";
+import { users } from "@/db/schema/users";
+
+const fakeUsers = [
+  { id: 1, username: "john_doe" },
+  { id: 2, username: "alice_smith" },
+  { id: 3, username: "michael_lei" },
+];
+
+const fakePosts = [
+  {
+    id: 1,
+    userId: 1,
+    title: "Exploring Artificial Intelligence",
+    content:
+      "Just finished reading a fascinating book about artificial intelligence. The author delves deep into the ethical implications of AI in our society. Highly recommended!",
+  },
+  {
+    id: 2,
+    userId: 2,
+    title: "Charming Coffee Shop Experience",
+    content:
+      "Visited a charming coffee shop today and tried their new caramel macchiato. The ambiance was cozy, and the coffee was simply delightful. Can't wait to go back!",
+  },
+  {
+    id: 3,
+    userId: 3,
+    title: "Weekend Hiking Adventure",
+    content:
+      "Spent the weekend hiking in the mountains. The scenic views were breathtaking, and the fresh mountain air was invigorating. Nature truly has a way of rejuvenating the soul.",
+  },
+];
+
+async function seedDatabase() {
+  await db.insert(users).values(fakeUsers);
+  await db.insert(posts).values(fakePosts);
+}
+
+seedDatabase()
+  .then(() => console.log("Database seeded!"))
+  .catch(() => console.error("Databse Error: Database seeding failed!"));
