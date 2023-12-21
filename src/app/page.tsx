f9982e4ffc64800affec3355f08f00d382d5c4d5
Michael <michaelleie0ca@gmail.com>
2023-12-20 20:13:21 -0800

    Refactor into post list and post item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { PencilIcon } from "lucide-react";
 
+function PostItem() {
+  return (
+    <div>
+      <p className="mb-5 text-xs text-gray-500 tracking-widest">10.11.18</p>
+      <article className="p-10 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <h2 className="font-semibold mb-2">Lorem ipsum dolor sit amet.</h2>
+        <p className="text-sm text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          voluptatum?
+        </p>
+      </article>
+    </div>
+  );
+}
+
+function PostList() {
+  return (
+    <div className="space-y-10 mb-10">
+      {Array.from({ length: 5 }).map((_, i) => (
+        <PostItem key={i} />
+      ))}
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <MaxWidthWrapper>
@@ -8,18 +33,7 @@ export default function Home() {
         <PencilIcon className="w-6 h-6" />
         <p className="text-lg font-semibold font-">Your feed</p>
       </div>
-      <div className="space-y-2">
-        <div>
-          <p className="mb-5 text-xs text-gray-500 tracking-widest">10.11.18</p>
-          <div className="p-10 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-            <h2 className="font-semibold mb-2">Lorem ipsum dolor sit amet.</h2>
-            <p className="text-sm text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              voluptatum?
-            </p>
-          </div>
-        </div>
-      </div>
+      <PostList />
     </MaxWidthWrapper>
   );
 }
