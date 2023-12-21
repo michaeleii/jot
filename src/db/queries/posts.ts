57bb096597ccf08ae36e9e17b19f272baaa4fd5b
Michael <michaelleie0ca@gmail.com>
2023-12-21 01:05:54 -0800

    Add queries for querying post table in db

---
+++ src/db/queries/posts.ts
@@ -0,0 +1,27 @@
+import { db, desc, eq, sql } from "..";
+import { posts } from "../schema/posts";
+import { users } from "../schema/users";
+
+const baseQuery = db
+  .select({
+    id: posts.id,
+    title: posts.title,
+    content: posts.content,
+    user: { id: users.id, username: users.username },
+    createdAt: posts.createdAt,
+  })
+  .from(posts)
+  .innerJoin(users, eq(users.id, posts.userId));
+
+export const homeFeedQuery = baseQuery.orderBy(desc(posts.createdAt)).prepare();
+
+export const userFeedQuery = baseQuery
+  .where(eq(users.id, sql.placeholder("userId")))
+  .orderBy(desc(posts.createdAt))
+  .prepare();
+
+export const singlePostQuery = baseQuery.where(
+  eq(posts.id, sql.placeholder("postId")),
+);
+
+export type Post = Awaited<ReturnType<typeof homeFeedQuery.all>>[0];
