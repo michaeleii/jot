7fd6acc972549bc7274ebcb2e79eedfbd3aeff04
Michael <michaelleie0ca@gmail.com>
2023-12-21 21:14:04 -0800

    Add user avatar to user profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,18 +1,29 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { userProfileQuery } from "@/db/queries/users";
+import Image from "next/image";
 
 async function Profile() {
   const user = await userProfileQuery
     .all({ userId: 1 })
     .then((users) => users[0]);
   return (
-    <article className="card">
-      <h1 className="main-heading">{user.username}</h1>
-      <p className="mt-10 text-muted-foreground">
-        {user.bio.length > 0
-          ? user.bio
-          : `${user.username} does not have a bio.`}
-      </p>
+    <article className="card flex flex-wrap justify-between">
+      <div>
+        <h1 className="main-heading">{user.username}</h1>
+        <p className="mt-10 text-muted-foreground">
+          {user.bio.length > 0
+            ? user.bio
+            : `${user.username} does not have a bio.`}
+        </p>
+      </div>
+      <Image
+        className="rounded-full"
+        src={user.avatar}
+        alt={user.username}
+        width={100}
+        height={100}
+        quality={100}
+      />
     </article>
   );
 }
