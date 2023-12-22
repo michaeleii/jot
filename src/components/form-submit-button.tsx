48e2e6692084bcd6a7792ac680a08a5e19c37a98
Michael <michaelleie0ca@gmail.com>
2023-12-21 19:22:51 -0800

    Add form submit button
    This will be a reusable component that can be used in other forms

---
+++ src/components/form-submit-button.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Loader2Icon } from "lucide-react";
+import { useFormStatus } from "react-dom";
+
+export default function FormSubmitButton({
+  value,
+  loadingValue,
+  variant = "default",
+}: {
+  value: React.ReactNode;
+  loadingValue: React.ReactNode;
+  variant?:
+    | "default"
+    | "secondary"
+    | "destructive"
+    | "outline"
+    | "ghost"
+    | "link"
+    | null
+    | undefined;
+}) {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      size="lg"
+      variant={variant}
+      type="submit"
+      className="w-full"
+      disabled={pending}
+    >
+      {pending ? (
+        <div className="flex items-center gap-2">
+          <Loader2Icon className="animate-spin" />
+          <span>{loadingValue}</span>
+        </div>
+      ) : (
+        value
+      )}
+    </Button>
+  );
+}
