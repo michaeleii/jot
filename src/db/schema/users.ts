a4e1627ac697f275953ba17255ff1a7b42d7811c
Michael <michaelleie0ca@gmail.com>
2023-12-21 20:53:47 -0800

    Add bio to user schema

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -4,6 +4,7 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const users = sqliteTable("users", {
   id: integer("id").notNull().primaryKey(),
   username: text("username").notNull().unique(),
+  bio: text("bio").notNull().default(""),
   createdAt: text("created_at")
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
