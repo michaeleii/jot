1904894065c664202e598b5a5bc3714d2843d57d
Michael <michaelleie0ca@gmail.com>
2023-12-23 01:33:44 -0800

    Show delete button if user created the post

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,6 +1,8 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
-import { singlePostQuery } from "@/db/queries/posts";
 import DeletePostDialog from "./delete-post-dialog";
+
+import { singlePostQuery } from "@/db/queries/posts";
+import { cookies } from "next/headers";
 
 type SinglePostItemProps = {
   postId: number;
@@ -8,10 +10,12 @@ type SinglePostItemProps = {
 
 async function SinglePostItem({ postId }: SinglePostItemProps) {
   const post = await singlePostQuery.all({ postId }).then((posts) => posts[0]);
+  const userId = cookies().get("user_id")?.value;
+  const isOwner = userId === post.user.id;
   return (
     <article className="px-5">
       <div className="flex justify-end">
-        <DeletePostDialog postId={postId} />
+        {isOwner && <DeletePostDialog postId={postId} />}
       </div>
       <h1 className="main-heading mt-10">{post.title}</h1>
       <span className="mb-5 text-sm text-muted-foreground">
