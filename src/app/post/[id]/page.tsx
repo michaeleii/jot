83b837a2f6c51ed3aed10a361d6552fec595ac94
Michael <michaelleie0ca@gmail.com>
2023-12-23 02:06:42 -0800

    Add share and like icon

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -3,6 +3,8 @@ import DeletePostDialog from "./delete-post-dialog";
 
 import { singlePostQuery } from "@/db/queries/posts";
 import { cookies } from "next/headers";
+import { HeartIcon, Share2Icon, ShareIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 type SinglePostItemProps = {
   postId: number;
@@ -14,14 +16,20 @@ async function SinglePostItem({ postId }: SinglePostItemProps) {
   const isOwner = userId === post.user.id;
   return (
     <article className="px-5">
-      <div className="flex justify-end">
-        {isOwner && <DeletePostDialog postId={postId} />}
-      </div>
-      <h1 className="main-heading mt-10">{post.title}</h1>
+      <h1 className="main-heading mt-5">{post.title}</h1>
       <span className="mb-5 text-sm text-muted-foreground">
         By {post.user.username}
       </span>
       <p className="mt-10 text-muted-foreground">{post.content}</p>
+      <div className="mt-10 flex items-center justify-end gap-2">
+        <Button variant="ghost" size="icon">
+          <HeartIcon className="h-6 w-6" />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Share2Icon className="h-6 w-6" />
+        </Button>
+        {isOwner && <DeletePostDialog postId={postId} />}
+      </div>
     </article>
   );
 }
