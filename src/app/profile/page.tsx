5726d1aeced052f928cc6c75968ae333efa19e72
Michael <michaelleie0ca@gmail.com>
2023-12-27 20:24:34 -0800

    Add post display for users profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,20 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { Profile } from "./profile";
-import SignOutForm from "./signout-form";
+import { userFeedQuery } from "@/db/queries/posts";
+import { getCurrentUser } from "@/lib/auth";
+import PostList from "@/components/post-list";
+
+async function ProfilePostsList() {
+  const user = await getCurrentUser();
+  const posts = await userFeedQuery.all({ userId: user.id });
+  return <PostList posts={posts} />;
+}
 
 export default function ProfilePage() {
   return (
     <MaxWidthWrapper>
       <Profile />
-      <SignOutForm />
+      <ProfilePostsList />
     </MaxWidthWrapper>
   );
 }
