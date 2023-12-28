c7b78f4912848cf32bf0a3b48fca2279110e60c3
Michael <michaelleie0ca@gmail.com>
2023-12-27 21:39:53 -0800

    Capitalize Google

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
-import google from "next-auth/providers/google";
+import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 import { db } from "./db";
 
 export const { handlers, auth, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [google],
+  providers: [Google],
   callbacks: {
     async session({ session, user }) {
       session.user.id = user.id;
